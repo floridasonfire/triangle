3ac3b273a17a8db22fe81bcddc846405acd06ef5
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 12:14:30 -0700

    create basic files and run first spec on program

---
+++ lib/triangle.rb
@@ -0,0 +1,11 @@
+class Triangle 
+  define_method(:initialize) do |side1, side2, side3|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  define_method(:triangle?) do
+    false
+  end
+end
---
+++ spec/triangle_spec.rb
@@ -0,0 +1,15 @@
+require('rspec')
+require('triangle')
+
+describe(Triangle) do
+  describe("#triangle?") do
+    it("returns false if it is not a triangle") do
+      test_triangle = Triangle.new(1, 2, 5)
+      expect(test_triangle.triangle?()).to(eq(false))
+    end
+  end
+end
+
+
+
+# it("tells us whether three lengths can make a triangle") do
