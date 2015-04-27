701cb4a3739f2243dceb934b41d86900b655b16b
Jessica Engel & Franz Knupfer <student@epicodus.com>
2015-04-27 13:23:09 -0700

    triangle program now works with an equilateral triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,11 +5,13 @@ class Triangle
     @side3 = side3
   end
 
-  define_method(:triangle?) do
+  define_method(:triangle) do
     if @side1.>(@side2.+(@side3))||@side2.>(@side1.+(@side3))||@side3.>(@side1.+(@side2))
       "This is not a triangle."
+    elsif @side1==@side2 && @side1==@side3
+      "This is an equilateral triangle."
     else
-      "This is a triangle."
+      "okay"
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,10 +2,14 @@ require('rspec')
 require('triangle')
 
 describe(Triangle) do
-  describe("#triangle?") do
-    it("returns false if it is not a triangle") do
+  describe("#triangle") do
+    it("returns that it's not a triangle if it is not a triangle") do
       test_triangle = Triangle.new(1, 2, 5)
-      expect(test_triangle.triangle?()).to(eq("This is not a triangle."))
+      expect(test_triangle.triangle()).to(eq("This is not a triangle."))
+    end
+    it("returns whether or not it's not equilateral triangle") do
+      test_triangle = Triangle.new(3, 3, 3)
+      expect(test_triangle.triangle()).to(eq("This is an equilateral triangle."))
     end
   end
 end
